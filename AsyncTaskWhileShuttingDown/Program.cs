5dadce592273b3382b39933666a683da7db4ef58
b-dmitry1 <93917607+b-dmitry1@users.noreply.github.com>
2022-02-21 00:09:07 +0300

    Update Program.cs

--- AsyncTaskWhileShuttingDown/Program.cs
+++ AsyncTaskWhileShuttingDown/Program.cs
@@ -1,16 +1,25 @@
-﻿using Microsoft.Win32;
+using Microsoft.Win32;
 
 // При завершении пользовательской сессии нам надо отправить данные на сервер при помощи асинхронного API,
 // реализованного в виде метода async Task SendData.
 // Задача: модифицировать код таким образом, чтобы данные успели записаться в файл до завершения сессии.
 // Изменять код метода SendData нельзя, заменять его другим методом нельзя.
 
+EventWaitHandle ev = new EventWaitHandle(false, EventResetMode.AutoReset);
+
+(new Thread(() =>
+{
+    ev.WaitOne();
+})).Start();
 
 Console.WriteLine("Disconnect, lock, reboot or shutdown to save your time.");
 
 SystemEvents.SessionEnding += SystemEvents_SessionEnding;
 SystemEvents.SessionSwitch += SystemEvents_SessionSwintc;
 
+// Чтобы можно было отпределить, когда пользователь вернулся в систему
+// после Lock или после смены пользователя
+SystemEvents.PowerModeChanged += SystemEvents_PowerModeChanged;
 
 
 while (true)
@@ -22,6 +31,10 @@ while (true)
     }
 }
 
+// Нужно либо выполнить SaveTime, либо ev.Set();
+// иначе программа не завершится
+SaveTime("Program quit");
+
 void SystemEvents_SessionEnding(object sender, SessionEndingEventArgs e)
 {
     SaveTime(e.Reason.ToString());
@@ -32,21 +45,36 @@ void SystemEvents_SessionSwintc(object sender, SessionSwitchEventArgs e)
     SaveTime(e.Reason.ToString());
 }
 
+void SystemEvents_PowerModeChanged(object sender, PowerModeChangedEventArgs e)
+{
+    SaveTime(e.Mode.ToString());
+}
+
+void Log(string s)
+{
+    File.AppendAllText("out.txt", $"{s}\n");
+}
+
 async Task SendData(string reason)
 {
     // connect to service
     await Task.Delay(500);
 
+    // Здесь нужно уложиться в 30 секунд, иначе появится сообщение о зависшей задаче,
+    // и Windows предложит завершить ее вручную
+
     // send data
-    File.AppendAllText("out.txt", $"Time: {DateTime.Now} Reason: {reason}");
+    Log($"Time: {DateTime.Now} Reason: {reason}\n");
 }
 
 void SaveTime(string reason)
 {
-    File.AppendAllText("out.txt", $"{DateTime.Now} Starting to send data.");
+    Log($"{DateTime.Now} Starting to send data.\n");
 
-    Task.Run(async() => await SendData(reason));
-    
-    SystemEvents.SessionEnding -= SystemEvents_SessionEnding;
-    SystemEvents.SessionSwitch -= SystemEvents_SessionSwintc;
+    SendData(reason).Wait();
+
+    // Данные сохранены, можно завершить guard thread
+    // На самом деле здесь не всегда надо завершать поток!
+    // Если пользователь заблокировал сеанс, то программа продолжит работать
+    ev.Set();
 }
